c226f3293bcaf25560cea7f74d647ab095dcee8e
Akshit Kumar <akshitk31.04@gmail.com>
2025-05-22 07:46:02 +0400

    Update cleaning_data.py

--- cleaning_data.py
+++ cleaning_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 df = pd.read_csv("car data.csv")
 df = df.drop(columns=["Car_Name"], errors='ignore') # dropping car name column, as its useless
 df = df.drop_duplicates() # removing dulpicate rows
+df = df.reset_index(drop=True) # resets count
 
 cat_cols = ["Fuel_Type", "Selling_type", "Transmission"]
 num_cols = [col for col in df.columns if col not in cat_cols + ["Year", "Selling_Price"]]
@@ -27,7 +28,6 @@ df = df.drop(columns=["Year"])
 # remove extreme outliers in Selling_Price 
 price_threshold = df["Selling_Price"].quantile(0.95)
 df = df[df["Selling_Price"] < price_threshold]
-
 
 df.to_csv("cleaned_car_data.csv", index=False)
 print("Data has been cleaned & saved as 'cleaned_car_data.csv'")
